bafe455c158d2cfa2ee7b85fc4a43d93c266f711
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-05-20 18:19:31 +0200

    Updates

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,5 +1,6 @@
-import React from "react";
-
+import React, { useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 import {
   Section,
   SectionDivider,
@@ -8,18 +9,42 @@ import {
 import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 import { data } from "../../constants/constants";
 
-const Acomplishments = () => (
-  <Section>
-    <SectionTitle>Personal Acomplishments</SectionTitle>
-    <Boxes>
-      {data.map((card, index) => (
-        <Box key={index}>
-          <BoxNum>{card.number}+</BoxNum>
-          <BoxText>{card.text}</BoxText>
-        </Box>
-      ))}
-    </Boxes>
-  </Section>
-);
+const AnimatedBox = motion(Box);
+
+const Acomplishments = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  return (
+    <Section>
+      <SectionTitle>Personal Accomplishments</SectionTitle>
+      <Boxes ref={ref}>
+        {data.map((card, index) => (
+          <AnimatedBox
+            key={index}
+            initial="hidden"
+            animate={controls}
+            transition={{ duration: 0.6, delay: index * 0.2 }}
+            variants={{
+              visible: { opacity: 1, y: 0 },
+              hidden: { opacity: 0, y: 50 },
+            }}
+          >
+            <BoxNum>{card.number}+</BoxNum>
+            <BoxText>{card.text}</BoxText>
+          </AnimatedBox>
+        ))}
+      </Boxes>
+    </Section>
+  );
+};
 
 export default Acomplishments;
